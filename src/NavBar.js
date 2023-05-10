afc2d379c8bcb95a65ed22e8b1595dfc2d7d3774
RegisLewis <regisaslewis@gmail.com>
2023-05-09 22:24:21 -0700

    starting to build

---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="title">
+      <h1>The Task Thrasher</h1>
+      <h2 className="squash">self-motivation <span className="lil">through</span> data management</h2>
+    </div>
+  )
+}
+
+export default NavBar;
