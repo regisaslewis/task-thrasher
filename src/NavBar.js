4f7106f5a746cc157984bcbc812a2b4691e13ac2
RegisLewis <regisaslewis@gmail.com>
2023-05-09 23:52:09 -0700

    adding links

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function NavBar() { 
   return (
@@ -7,6 +8,20 @@ function NavBar() {
         <h1>The Task Thrasher</h1>
         <h2 className="squash">self-motivation <span className="lil">through</span> data management</h2>
       </div>
+        <div className="button-lineup">
+          <NavLink to="/" exact>
+            <button className="nav-button">Home</button>
+          </NavLink>
+          <NavLink to="/points" exact>
+            <button className="nav-button">Points</button>
+          </NavLink>
+          <NavLink to="/review" exact>
+            <button className="nav-button">Review</button>
+          </NavLink>
+          <NavLink to="/Tasks" exact>
+            <button className="nav-button">Tasks</button>
+          </NavLink>
+        </div>
     </>
   )
 }
